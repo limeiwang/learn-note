714c27bb8479f88a1da7780cc79d2f9027295d97
李美旺 <17803125899@163.com>
2020-11-17 21:39:32 +0800

    test

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -140,7 +140,7 @@ module.exports = {
                     {
                         title: '面试问题',   // 必要的
                         path: '/other/interview',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
-                    }
+                    },
                 ]
             },
             '/Node', 
